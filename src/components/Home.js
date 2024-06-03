6eb3027b857a6b8024367720061dc94861a0e716
Suprith Singh <123349613+suprith03@users.noreply.github.com>
2024-06-03 15:44:35 +0530

    site under Process

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ const Home = () => {
       <p>Hi, I'm a passionate developer with a love for creating functional web applications.</p>
       <p>I am Highly motivated Computer Science student with a strong foundation in programming languages. Seeking to expand knowledge in web applications, computer networks, and security. Eager to leverage my skills and problem-solving abilities to contribute to a collaborative team environment.</p>
       <a href="/Resume.pdf" className="btn" download>Resume</a>
+      <p>- - - - - - - - - - - -This websites is still under process , kindly wait for the final results- - - - - - - - - - </p>
     </section>
   );
 };
